0d0757c0ad877413d4799a6cb33e0cb3cd53d723
Miguel Ferreira <migueltec3@gmail.com>
2018-04-22 11:24:11 +0200

    PromesaId.java
    Se crea la tabla PromesaId de soporte para la clase promesa con las claves compuestas.

---
+++ PCera/src/main/java/com/mycompany/pcera/PromesaId.java
@@ -0,0 +1,10 @@
+package com.mycompany.pcera
+import java.util.Date;
+@Embeddable
+public class PromesaId implements Serializable {
+@Column (name="PK_usuario_id")
+privateLong Id_Usuario;
+@Column (name="PK_seccion_id")
+private Long Id_Seccion ;
+
+}
